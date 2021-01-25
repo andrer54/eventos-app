f21e8914c6d3c4bb4deaae637bba66dee4694385
andrer54 <andrer54@gmail.com>
2021-01-25 12:10:53 -0200

    add adapter ORM

--- src/main/java/com/todoapp/DataConfiguration.java
+++ src/main/java/com/todoapp/DataConfiguration.java
@@ -9,9 +9,9 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.jdbc.datasource.DriverManagerDataSource;
-//import org.springframework.orm.jpa.JpaVendorAdapter;
-//import org.springframework.orm.jpa.vendor.Database;
-//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfiguration {
@@ -30,6 +30,16 @@ public class DataConfiguration {
 
         return basicDataSource;
     }
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+        }
 }
 
 /*
